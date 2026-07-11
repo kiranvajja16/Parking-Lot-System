00775b0d1a00af47023f6e751e9dbc64185ddb6d
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-11 18:06:21 +0530

    getslots function is updated

--- backend/controllers/parkingController.js
+++ backend/controllers/parkingController.js
@@ -1,18 +1,55 @@
+const db = require('../config/db');
+
+const LIMITS = {
+    bike :5,
+    car : 5,
+    truck : 2,
+};
+
+
 const getSlots=(req,res)=>{
-    res.json({
-        bike:{
-            total: 5,
-            available:5,
-        },
-        car:{
-            total: 5,
-            available: 5,
-        },
-        truck:{
-            total:2,
-            available: 2,
-        },
-    });
+    const sql=`
+    SELECT vechile_type, COUNT(*) AS occupied
+    FROM tickets 
+    WHERE status = 'parked' GROUP BY
+    vechile_type
+    `;
+
+    db.query(sql,(err,results)=>{
+        if(err){
+            return res.status(500).json({
+                success : false,
+                message : "Database Error",
+                error: err.message,
+            });
+        }
+
+        const occupied = {
+            bike : 0,
+            car : 0,
+            truck : 0,
+        };
+
+        results.forEach((row)=>{
+            occupied[row.vechile_type]= row.occupied;
+        });
+        
+        const response = {
+            bike:{
+                total :  LIMITS.bike,
+                available : LIMITS.bike - occupied.bike,
+            },
+            car:{
+                total : LIMITS.car,
+                available : LIMITS.car - occupied.car,
+            },
+            truck:{
+                total : LIMITS.truck,
+                available : LIMITS.truck - occupied.truck,
+            },
+        };
+        res.json(response);
+    });    
 };
 
 module.exports={getSlots};
--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api',parkingRoutes);
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT);
 })

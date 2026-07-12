ad2ecd55d05a7577b055c09bb6e2bfdc24cab06b
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-12 15:19:48 +0530

    updated the port

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,22 @@
-const mysql=require('mysql2');
-const dotenv=require('dotenv');
-dotenv.config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-const db=mysql.createConnection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-db.connect((err)=>{
-    if(err){
-        console.error('Database Connection Failed');
+db.connect((err) => {
+    if (err) {
+        console.error("Database Connection Failed");
         console.error(err);
         return;
     }
-    console.log('MySql Database Connected');
+
+    console.log("MySql Database Connected");
 });
 
-module.exports=db;
+module.exports = db;

a64f4a50fc46c31359081179c52730d97175f756
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-12 16:10:10 +0530

    UPDated to chekcing the deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,   
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -11,11 +16,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) {
-        console.error("Database Connection Failed");
         console.error(err);
         return;
     }
-
     console.log("MySql Database Connected");
 });
 
